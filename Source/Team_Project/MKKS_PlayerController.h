40514aca2e06710ab79ade7fc9713e21a157cf77
Sungju_Moon <msjj5562@gmail.com>
2022-07-04 20:36:57 +0900

    Make many

---
+++ Source/Team_Project/MKKS_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MKKS_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM_PROJECT_API AMKKS_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
