98a714c7c3a9a76f71d539075705c9fdd8ecbc40
Sungju_Moon <msjj5562@gmail.com>
2022-07-04 19:27:11 +0900

    Add project files.

---
+++ Source/Team_Project/Team_Project.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Team_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team_Project, "Team_Project" );
+ 
