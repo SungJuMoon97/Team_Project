554975fc8e607d11db9615009810cd9db6c77e6a
Sungju_Moon <msjj5562@gmail.com>
2022-07-05 10:23:16 +0900

    Weapon Download

--- Source/Team_Project/MKKS_PlayerAnimInstance.h
+++ Source/Team_Project/MKKS_PlayerAnimInstance.h
@@ -13,5 +13,30 @@ UCLASS()
 class TEAM_PROJECT_API UMKKS_PlayerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Attack,
+		meta = (AllowPrivateAccess = "true"))
+		bool bDoAttacking;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement,
+		meta = (AllowPrivateAccess = "true"))
+		bool bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement,
+		meta = (AllowPrivateAccess = "true"))
+		bool bIsSprint;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement,
+		meta = (AllowPrivateAccess = "true"))
+		bool bSitting;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement,
+		meta = (AllowPrivateAccess = "true"))
+		bool bLyingDown;
+
+
+
 };
