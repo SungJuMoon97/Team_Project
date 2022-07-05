628f348f62490db008f1bd62f6791f11fb7f457c
Sungju_Moon <msjj5562@gmail.com>
2022-07-05 10:21:54 +0900

    Make Enum Make AnimInstance
    Weapon Download

---
+++ Intermediate/ProjectFiles/Enum_Collection.h
@@ -0,0 +1,10 @@
+#pragma once
+
+UENUM(BlueprintType)
+enum class WeaponType :uint32
+{
+	EWT_Fist UMETA(DisplayName = "Fist"),
+	EWT_Sword UMETA(DisplayName = "Sword"),
+	EWT_Shield UMETA(DisplayName = "Shield"),
+	EWT_Fist UMETA(DisplayName = "Fist"),
+};
