ade41cf25eef9a5da8bd5132ccd306cae1c6e8b1
Sungju_Moon <msjj5562@gmail.com>
2022-07-18 16:37:27 +0900

    123

--- Source/Team_Project/Team_ProjectCharacter.h
+++ Source/Team_Project/Team_ProjectCharacter.h
@@ -19,9 +19,6 @@ class ATeam_ProjectCharacter : public ACharacter
 public:
 	ATeam_ProjectCharacter();
 
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
-
 	void LeftHand();
 	void RightHand();
 
@@ -71,37 +68,17 @@ public:
 
 protected:
 
-	/** Called for forwards/backward input */
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
 	void MoveForward(float Value);
-
-	/** Called for side to side input */
 	void MoveRight(float Value);
-
-	/** 
-	 * Called via input to turn at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
 	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
 	void LookUpAtRate(float Rate);
 
-	/** Handler for when a touch input begins. */
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
-
-	/** Handler for when a touch input stops. */
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
-
-	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	// End of APawn interface
 
 	void Interact();
-
-
 private:
 	/** Camera boom positioning the camera behind the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
