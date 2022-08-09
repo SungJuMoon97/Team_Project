ced167b8639f46693aaf4a1b636fac155bbf7d43
Sungju_Moon <msjj5562@gmail.com>
2022-08-09 13:04:11 +0900

    123

--- Source/Team_Project/Team_ProjectCharacter.cpp
+++ Source/Team_Project/Team_ProjectCharacter.cpp
@@ -38,20 +38,7 @@ ATeam_ProjectCharacter::ATeam_ProjectCharacter() :
 	CurrentStanding(EStanding::ESD_Standing),
 	bSitting(false), bLayingDown(false), bCrouching(false),
 	inputTime(2.0f),
-	bIsInventoryOpen(false),
-	//PlayerStat
-	Health(100.f),MaxStamina(1.0f), MaxHungry(100.0f),MaxThirsty(100.0f),
-	currentStamina(1.0f),staminaSprintUsageRate(0.05f),staminaRechargeRate(0.01f),
-	CurrentHungry(100.0f), CurrentThirsty(100.f),HungryRate(0.5f),ThirstyRate(0.8f),
-	FoodWaterDrainRate(10.0f),//배고픔목마름줄어드는시간
-	//BowAiming Setting
-	CameraCurrentFOV(90.0f), CameraDefaultFOV(90.0f),CameraZoomedFOV(50.0f),ZoomInterpSpeed(20.0f),
-	MaxHealth(600.f), MaxHeadHealth(100.f), MaxBodyHealth(100.f), MaxRightArmHealth(100.f), MaxLeftArmHealth(100.f), MaxRightLegHealth(100.f), MaxLeftLegHealth(100.f),
-	//WeaponEquip
-	bLeftKnuckleEquip(false), bRightKnuckleEquip(false), bHammerEquip(false),bLeftSwordEquip(false), bRightSwordEquip(false),
-	bTwoHandedSwordEquip(false), bBowEquip(false),
-	CurrentHeadHealth(100.f), CurrentBodyHealth(100.f), CurrentRightArmHealth(100.f), CurrentLeftArmHealth(100.f), CurrentRightLegHealth(100.f), CurrentLeftLegHealth(100.f),
-	CurrentHealth(600.f)
+	bIsInventoryOpen(false)
 {
 	PrimaryActorTick.bCanEverTick = true;
 	// Set size for collision capsule
@@ -143,6 +130,15 @@ void ATeam_ProjectCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ATeam_ProjectCharacter::DecreaseFoodWater, FoodWaterDrainRate, true, 6.f);
+
+	if (IsLocallyControlled() && PlayerWidgetClass)
+	{
+		AMKKS_PlayerController* FPC = GetController<AMKKS_PlayerController>();
+		check(FPC);
+		PlayerWidget = CreateWidget<UBarWidget>(FPC, PlayerWidgetClass);
+		check(PlayerWidget);
+		PlayerWidget->AddToViewport();
+	}
 }
 
 void ATeam_ProjectCharacter::Tick(float DeltaTime)
@@ -431,52 +427,6 @@ void ATeam_ProjectCharacter::CameraOption()
 	}
 }
 
-void ATeam_ProjectCharacter::BlockModeAim()
-{
-}
-
-void ATeam_ProjectCharacter::BowAiming(float DeltaTime)
-{
-
-		if (bAiming)
-		{
-			//bAiming = true;
-			CameraCurrentFOV = FMath::FInterpTo(
-				CameraCurrentFOV,
-				CameraZoomedFOV,
-				DeltaTime,
-				ZoomInterpSpeed);
-		}
-		else
-		{
-			//bAiming = false;
-			CameraCurrentFOV = FMath::FInterpTo(
-				CameraCurrentFOV,
-				CameraDefaultFOV,
-				DeltaTime,
-				ZoomInterpSpeed);
-		}
-		if (CurrentViewMode == EViewType::EVT_FirstPerson)
-		{
-			GetFirstPersonFollowCamera()->SetFieldOfView(CameraCurrentFOV);
-		}
-}
-
-void ATeam_ProjectCharacter::BarWidget()
-{
-	if (IsLocallyControlled() && PlayerWidgetClass)
-	{
-		AMKKS_PlayerController* FPC = GetController<AMKKS_PlayerController>();
-		check(FPC);
-		PlayerWidget = CreateWidget<UBarWidget>(FPC, PlayerWidgetClass);
-		check(PlayerWidget);
-		PlayerWidget->AddToViewport();
-		CurrentHealth = (MaxHeadHealth + MaxBodyHealth + MaxRightArmHealth + MaxLeftArmHealth + MaxRightLegHealth + MaxLeftLegHealth);
-		PlayerWidget->SetHealth(CurrentHealth, MaxHealth);
-
-	}
-}
-
 void ATeam_ProjectCharacter::SetViewType(EViewType ViewType)
 {
 	
@@ -707,8 +657,11 @@ void ATeam_ProjectCharacter::SetupPlayerInputComponent(class UInputComponent* Pl
 	PlayerInputComponent->BindAction("ViewChange", IE_Pressed, this, &ATeam_ProjectCharacter::ViewChange);
 	PlayerInputComponent->BindAction("StanceChange", IE_Pressed, this, &ATeam_ProjectCharacter::StanceChange);
 	PlayerInputComponent->BindAction("StandingChange", IE_Released, this, &ATeam_ProjectCharacter::StandingChange);
+	// Item Interact binding (Press E)
 	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &ATeam_ProjectCharacter::Interact);
+	// AddToInventory binding
 	PlayerInputComponent->BindAction("AddToInventory", IE_Pressed, this, &ATeam_ProjectCharacter::AddToInventory);
+	// OpenInventory binding
 	PlayerInputComponent->BindAction("OpenInventory", IE_Pressed, this, &ATeam_ProjectCharacter::OpenInventory);
 	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &ATeam_ProjectCharacter::SprintStart);
 	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &ATeam_ProjectCharacter::SprintEnd);
@@ -753,124 +706,11 @@ void ATeam_ProjectCharacter::GrabActor()
 
 	if (bResult)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Hit Actor"));
-		TraceItem = Cast<AItem>(HitResult.GetActor());
-		TraceWeapon = Cast<AWeapon>(HitResult.GetActor());
-		FName HandSocket(TEXT("hand_r_Socket"));
+		UE_LOG(LogTemp, Warning, TEXT("HIT ACTOR"));
 
-		if (UGameplayStatics::GetPlayerController(GetWorld(), 0)->IsInputKeyDown(EKeys::LeftMouseButton))
+		if (AActor* HitActor = HitResult.GetActor())
 		{
-			//LeftHand
-			const USkeletalMeshSocket* LeftHandSocket = GetMesh()->GetSocketByName(FName("hand_l_Socket"));
-
-			if (TraceWeapon)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("This is LeftWeapon"));
-				TraceWeapon->SetItemState(EItemState::EIS_Equip);
-				//TraceWeapon->GetItemMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-				//이게 되야 캐릭터가 템이랑 메쉬가 블록처리되지 않음(SetItemState 에 들어있음)
-				if (TraceWeapon->GetOneHandedEquip() && TraceWeapon->GetFistWeapon())
-				{
-					const USkeletalMeshSocket* FistSocket = GetMesh()->GetSocketByName(FName("hand_l_FSocket"));
-					FistSocket->AttachActor(TraceWeapon, GetMesh());
-					bLeftKnuckleEquip = true;
-					//GrabbedTRCheck();
-				}
-				else if((TraceWeapon->GetOneHandedEquip() && TraceWeapon->GetSwordWeapon()) || 
-					(TraceWeapon->GetTwoHandedEquip()&&TraceWeapon->GetHammerWeapon()))
-				{
-					LeftHandSocket->AttachActor(TraceWeapon, GetMesh());
-					if (TraceWeapon->GetOneHandedEquip() && TraceWeapon->GetSwordWeapon())
-					{
-						bLeftSwordEquip = true;
-						//GrabbedTRCheck();
-					}
-					else
-					{
-						bHammerEquip = true;
-						//GrabbedRightActorLTCheck();
-					}
-				}
-				else if (TraceWeapon->GetTwoHandedEquip() && TraceWeapon->GetSwordWeapon())
-				{
-					const USkeletalMeshSocket* TSSocket = GetMesh()->GetSocketByName(FName("hand_l_TSSocket"));
-					TSSocket->AttachActor(TraceWeapon, GetMesh());
-					bTwoHandedSwordEquip = true;
-					//GrabbedRightActorLTCheck();
-				}
-				else if (TraceWeapon->GetBowEquip() && TraceWeapon->GetBowWeapon())
-				{
-					const USkeletalMeshSocket* BowSocket = GetMesh()->GetSocketByName(FName("hand_l_BSocket"));
-					BowSocket->AttachActor(TraceWeapon, GetMesh());
-					bBowEquip = true;
-					//GrabbedTRCheck();
-				}
-				LeftEquippedWeapon = TraceWeapon;
-				bLeftWeaponEquip = true;
-				bWeaponIsLeftHand = true;
-			}
-			else if (TraceItem)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("This is LeftItem"));
-				TraceItem->SetItemState(EItemState::EIS_Equip);
-				LeftHandSocket->AttachActor(TraceItem, GetMesh());
-				LeftEquippedItem = TraceItem;
-				bLeftItemEquip = true;
-				//GrabbedTRCheck();
-			}
-			
-			
-		}
-		else if (UGameplayStatics::GetPlayerController(GetWorld(), 0)->IsInputKeyDown(EKeys::RightMouseButton))
-		{
-			//RightHand
-			const USkeletalMeshSocket* RightHandSocket = GetMesh()->GetSocketByName(FName("hand_r_Socket"));
-
-			if (TraceWeapon)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("This is RightWeapon"));
-				TraceWeapon->SetItemState(EItemState::EIS_Equip);
-				if (TraceWeapon->GetOneHandedEquip() && TraceWeapon->GetFistWeapon())
-				{
-					const USkeletalMeshSocket* FistSocket = GetMesh()->GetSocketByName(FName("hand_r_FSocket"));
-					FistSocket->AttachActor(TraceWeapon, GetMesh());
-					bRightKnuckleEquip = true;
-					//GrabbedTLCheck();
-				}
-				else if ((TraceWeapon->GetOneHandedEquip() && TraceWeapon->GetSwordWeapon()) ||
-					(TraceWeapon->GetTwoHandedEquip() && TraceWeapon->GetHammerWeapon()))
-				{
-					RightHandSocket->AttachActor(TraceWeapon, GetMesh());
-					if (TraceWeapon->GetOneHandedEquip() && TraceWeapon->GetSwordWeapon())
-					{
-						bRightSwordEquip = true;
-						//GrabbedTLCheck();
-					}
-					else
-					{
-						bHammerEquip = true;
-						//GrabbedLeftActorRTCheck();
-					}
-				}
-				else if (TraceWeapon->GetTwoHandedEquip() && TraceWeapon->GetSwordWeapon())
-				{
-					const USkeletalMeshSocket* TSSocket = GetMesh()->GetSocketByName(FName("hand_r_TSSocket"));
-					TSSocket->AttachActor(TraceWeapon, GetMesh());
-					bTwoHandedSwordEquip = true;
-					//GrabbedLeftActorRTCheck();
-				}
-				else if (TraceWeapon->GetBowEquip() && TraceWeapon->GetBowWeapon())
-				{
-					const USkeletalMeshSocket* BowSocket = GetMesh()->GetSocketByName(FName("hand_r_BSocket"));
-					BowSocket->AttachActor(TraceWeapon, GetMesh());
-					bBowEquip = true;
-				}
-				RightEquippedWeapon = TraceWeapon;
-				bRightWeaponEquip = true;
-				bWeaponIsLeftHand = true;
-				//GrabbedTLCheck();
-			}
-			else if (TraceItem)
+			if (IPickup_Interface* Interface = Cast<IPickup_Interface>(HitActor))
 			{
 				UE_LOG(LogTemp, Warning, TEXT("This is RightItem"));
 				TraceItem->SetItemState(EItemState::EIS_Equip);
@@ -1365,6 +1205,11 @@ void ATeam_ProjectCharacter::PutActor()
 
 		if (AActor* HitActor = HitResult.GetActor())
 		{
+			if (IPickup_Interface* Interface = Cast<IPickup_Interface>(HitActor))
+			{
+				Interface->Puton();
+				// InventoryComponent->AddItemToInventory(HitActor);
+			}
 		}
 	}
 }
